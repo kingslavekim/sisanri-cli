8cbd9764d76afc2cec0158494ca668d26856642d
kingslavekim <r93211@gmail.com>
2022-10-24 13:01:00 +0900

    2022.10.24

--- src/Root.js
+++ src/Root.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import {HelloPage} from "./components";
 
 const Root = () => (
     <BrowserRouter>
         <App/>
+        <HelloPage/>
     </BrowserRouter>
 );
 
